f0b3c2033bd234340d579ab6b249570005503c54
FunnyGuy9796 <christophersciccarelli@gmail.com>
2024-10-01 22:50:20 -0500

    Minor cosmetic update

--- kernel.c
+++ kernel.c
@@ -5,9 +5,11 @@ void __attribute__((section(".text.entry"))) kmain(void) {
     
     vga_clear();
 
-    printf("Kernel loaded at memory address: %x\n", 0x10000);
-    printf("Welcome to %s, v%0.1f\n\n", "calcOS", 1.0);
+    printf("\n----------------------------------calcOS, v%0.1f----------------------------------\n", 1.0);
     printf("calcOS is a simple x86 operating system that uses a custom bootloader that is currently a work in progress. The goal of this project is to learn how operating systems and bootloaders work as well as to have fun.\n");
+
+    printf("\n\n----------------------------------SYSTEM INFO:----------------------------------\n");
+    printf("Kernel loaded at memory address: %x\n", 0x10000);
 
     while (1) {}
 }
